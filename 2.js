65afa408e3196fa9721c94545ef27be01fe9b584
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-01 20:33:15 +0500

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,2 @@
+var person = 'Mubasir';
+console.log("Hello ".concat(person, " do you live to study with me"));
---
+++ 2.ts
@@ -0,0 +1,3 @@
+let person:string='Mubasir';
+console.log(`Hello ${person} do you live to study with me`);
+
