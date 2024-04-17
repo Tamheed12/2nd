34c395557902ba48a4d397e6f99c1dfed0d77b03
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 12:57:06 +0500

    7th

---
+++ 7.js
@@ -0,0 +1,1 @@
+console.log("Addition => ".concat(7 + 1, "\n Subtraction => ").concat(9 - 1, "\n Multiplication => ").concat(2 * 4, "\n Division => ").concat(16 / 2));
---
+++ 7.ts
@@ -0,0 +1,4 @@
+console.log(`Addition => ${7+1}
+ Subtraction => ${9-1}
+ Multiplication => ${2*4}
+ Division => ${16/2}`);
