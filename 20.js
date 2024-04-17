d1ea9830e50191d1fefd383bf55e4148c4672245
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:04:16 +0500

    20th

---
+++ 20.js
@@ -0,0 +1,6 @@
+var subject = ['Chemistry', 'Physics', 'English', 'Biology', 'Math', 'Pakistan study'];
+console.log('\t These are subject i like to study\n');
+for (var _i = 0, subject_1 = subject; _i < subject_1.length; _i++) {
+    var top_1 = subject_1[_i];
+    console.log(top_1 && "Letter of subject name ".concat(top_1.length));
+}
---
+++ 20.ts
@@ -0,0 +1,4 @@
+let subject:string[]=['Chemistry','Physics','English','Biology','Math','Pakistan study'];
+console.log('\t These are subject i like to study\n')
+for(let top of subject)
+{console.log(top )}
