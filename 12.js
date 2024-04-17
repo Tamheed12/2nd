fd55182349c14948862db14895ef2a5afd3a96ff
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:00:52 +0500

    12th

---
+++ 12.js
@@ -0,0 +1,2 @@
+var friends = ['Mustafa', 'Afzal', 'Rizwan'];
+friends.forEach(function (friend) { console.log("Hello ".concat(friend, " do free at night lets go some where")); });
---
+++ 12.ts
@@ -0,0 +1,3 @@
+let friends:string[]=['Mustafa','Afzal','Rizwan'];
+friends.forEach(friend => 
+{console.log(`Hello ${friend} do free at night lets go some where`)});
