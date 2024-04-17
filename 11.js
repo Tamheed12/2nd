0ff85e19d1ef39b7283c8b8b263e97357290b0cb
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:00:22 +0500

    11th

---
+++ 11.js
@@ -0,0 +1,4 @@
+var names = ['Hamza', 'Atif', 'Asif'];
+console.log("\n ".concat(names[0]));
+console.log(names[1]);
+console.log(names[2]);
---
+++ 11.ts
@@ -0,0 +1,4 @@
+let names:string[]=['Hamza','Atif','Asif'];
+console.log(`\n${names[0]}`);
+console.log(names[1]);
+console.log(names[2]);
