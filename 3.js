464f83d9570eb255a7fe758ac8af1c4c86712dc2
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 12:06:54 +0500

    3rd

---
+++ 3.js
@@ -0,0 +1,2 @@
+var sir = "Nasir";
+console.log("LowerCase ".concat(sir.toLowerCase(), "  \n  \n UpperCase ").concat(sir.toUpperCase(), "\n  \n TitleCase ").concat(sir.charAt(0).toUpperCase() + sir.slice(1).toLowerCase()));
---
+++ 3.ts
@@ -0,0 +1,7 @@
+let sir: string = "Nasir";
+
+console.log(
+  ` LowerCase ${sir.toLowerCase()}  
+  \n UpperCase ${sir.toUpperCase()}
+  \n TitleCase ${sir.charAt(0).toUpperCase() + sir.slice(1).toLowerCase()}`
+);
