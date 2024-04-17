f9368b3a828a7916a238bb07181041db6d8372ee
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:05:23 +0500

    22th

---
+++ 22.js
@@ -0,0 +1,2 @@
+var list = ['lahore', 6, true, 'karachi', false, 7654, 'quetta'];
+console.log(list[23]);
---
+++ 22.ts
@@ -0,0 +1,4 @@
+
+let list:any[]=['lahore',6,true,'karachi',false,7654,'quetta'];
+
+console.log(list[23]);
