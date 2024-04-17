6d6e5e71d64a6028c3222f27aa075895d572c091
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:05:53 +0500

    23th

---
+++ 23.js
@@ -0,0 +1,31 @@
+// Randum qustan for you
+//test => 1
+var h = 'H';
+console.log("\ntest 1\nHydrogen atom is denoted by 'H'?\n    ".concat(h == 'H')); //true
+//test => 2
+var i = 'Karachi';
+console.log("\ntest 2\nPakistan biggest city is Lahore?\n    ".concat(i == 'Lahore')); //false
+//test => 3
+var g = 'coding';
+console.log("\ntest 3\nwhich skill need to make website?\n    ".concat(g === 'Language')); //false
+//test => 4
+var j = 'Yes' || 'No';
+console.log("\ntest 4\nYou are non-muslim?\n    ".concat(j == 'Yes'));
+//test => 5
+var l = 4;
+console.log("\ntest 5\nHow many legs animal have ? \n    ".concat(l <= 4));
+//test => 6
+var k = 2;
+console.log("\ntest 6\nHow many eye you have?\n    ".concat(k != 1));
+//test => 7
+var m = 'Blue' && 'Black';
+console.log("\ntest 7\nMain two color of sky?\n    ".concat(m == 'Blue' && 'Black'));
+//test => 8
+var n = '9';
+console.log("\ntest 8\nHow many letters in Education?\n    ".concat(n == '8'));
+//test => 9
+var t = 2.33;
+console.log("\ntest 9\nPi value?\n    ".concat(t == 2.34));
+//test => 10
+var s = 'Yes';
+console.log("\ntest 10\ndo you learn Gen AI?\n    ".concat(n == 'NO'));
---
+++ 23.ts
@@ -0,0 +1,64 @@
+
+                    // Randum qustan for you
+
+//test => 1
+let h:string='H';
+console.log(`\ntest 1
+Hydrogen atom is denoted by 'H'?
+    ${h=='H'}`);//true
+
+//test => 2
+let i:string='Karachi';
+console.log(`\ntest 2
+Pakistan biggest city is Lahore?
+    ${i=='Lahore'}`);//false
+
+//test => 3
+let g:string='coding';
+console.log(`\ntest 3
+which skill need to make website?
+    ${g==='Language'}`);//false
+
+//test => 4
+let j:string='Yes'||'No';
+console.log(`\ntest 4
+You are non-muslim?
+    ${j =='Yes'}`);//true
+
+//test => 5
+let l=4
+console.log(`\ntest 5
+How many legs animal have ? 
+    ${l<=4}`);//true
+
+//test => 6
+let k=2
+console.log(`\ntest 6
+How many eye you have?
+    ${k!=1}`);//true
+
+//test => 7
+let m:string='Blue'&&'Black'
+console.log(`\ntest 7
+Main two color of sky?
+    ${m=='Blue'&&'Black'}`);//false
+
+//test => 8
+let n:string='9'
+console.log(`\ntest 8
+How many letters in Education?
+    ${n=='8'}`);//false
+
+//test => 9
+let t = 2.33
+console.log(`\ntest 9
+Pi value?
+    ${t==2.34}`);//false
+
+//test => 10
+let s:string='Yes'
+console.log(`\ntest 10
+do you learn Gen AI?
+    ${n=='Yes'}`);//false
+
+
