b915de69d0ea06cf7f8f003572eb9a1ad2951237
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:04:50 +0500

    21th

---
+++ 21.js
@@ -0,0 +1,20 @@
+//list of team PSL 2024 with best player
+var karachi = { name: 'Karachi Kings',
+    bestPlayer: 'Imad Wasim' };
+var multan = { name: 'Multan Sultans',
+    bestPlayer: 'M.Rizwan' };
+var zalmi = { name: 'Peshawar Zalmi',
+    bestPlayer: 'Babar Azam' };
+var islamabad = { name: 'Islamabad United',
+    bestPlayer: 'Shadab Khan' };
+var lahore = { name: 'Lahore Qalandars',
+    bestPlayer: 'Shahin Afridi' };
+var quetta = { name: 'Quetta Gladiators',
+    bestPlayer: 'M.Nawaz' };
+console.log('\n\t  Psl 2024 Team list and Best player\n');
+console.log("1st team  '".concat(multan.name, "' and best player '").concat(multan.bestPlayer, "'"));
+console.log("2nd team '".concat(zalmi.name, "' and best player '").concat(zalmi.bestPlayer, "'"));
+console.log("3rd team '".concat(islamabad.name, "' and best player '").concat(islamabad.bestPlayer, "'"));
+console.log("4th team '".concat(quetta.name, "' and best player '").concat(quetta.bestPlayer, "'"));
+console.log("5th team '".concat(karachi.name, "' and best player '").concat(karachi.bestPlayer, "'"));
+console.log("6th team '".concat(lahore.name, "' and best player '").concat(lahore.bestPlayer, "'"));
---
+++ 21.ts
@@ -0,0 +1,49 @@
+//variable assign
+interface team
+    {name:string 
+        bestPlayer:string}
+
+        //list of team PSL 2024 with best player
+
+const karachi :team=
+    {name:'Karachi Kings',
+        bestPlayer:'Imad Wasim'}
+
+const multan :team=
+        {name:'Multan Sultans',
+            bestPlayer:'M.Rizwan'}
+
+const zalmi:team=
+    {name:'Peshawar Zalmi',
+        bestPlayer:'Babar Azam'}
+
+const islamabad:team=
+        {name:'Islamabad United',
+            bestPlayer:'Shadab Khan'}
+
+const lahore:team=
+        {name:'Lahore Qalandars',
+            bestPlayer:'Shahin Afridi'}
+
+const quetta:team=
+    {name:'Quetta Gladiators',
+        bestPlayer:'M.Nawaz'}
+
+console.log('\n\t  Psl 2024 Team list and Best player\n')
+
+console.log(`1st team  '${multan.name}' and best player '${multan.bestPlayer}'`)
+
+console.log(`2nd team '${zalmi.name}' and best player '${zalmi.bestPlayer}'`)
+
+console.log(`3rd team '${islamabad.name}' and best player '${islamabad.bestPlayer}'`)
+
+console.log(`4th team '${quetta.name}' and best player '${quetta.bestPlayer}'`)
+
+console.log(`5th team '${karachi.name}' and best player '${karachi.bestPlayer}'`)
+
+console.log(`6th team '${lahore.name}' and best player '${lahore.bestPlayer}'`)
+
+
+
+
+
