b2a94bda91599b7dac64f03050d5f2bb658e93a9
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 13:01:27 +0500

    13th

---
+++ 13.js
@@ -0,0 +1,2 @@
+var subjects = ['Chemistry', 'Physics', 'Maths', 'Biology'];
+subjects.map(function (sub) { return console.log("My favorite subjects are ".concat(sub)); });
---
+++ 13.ts
@@ -0,0 +1,3 @@
+let subjects:string[]=['Chemistry','Physics','Maths','Biology'];
+subjects.map((sub)=>console.log(`My favorite subjects are ${sub}`));
+
