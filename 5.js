61d488dd459e910f164831890b1614d76594b1ff
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-17 12:11:47 +0500

    5th

---
+++ 5.js
@@ -0,0 +1,2 @@
+var person = 'Albert Einstein';
+console.log("".concat(person, " once said, \u201CA person who never made a mistake never tried anything new."));
---
+++ 5.ts
@@ -0,0 +1,2 @@
+let person:string='Albert Einstein'
+console.log(`${person} once said, “A person who never made a mistake never tried anything new.`)
