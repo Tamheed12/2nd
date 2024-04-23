5850417aa9b866d325178fe74d0a89ff225fbef4
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-23 20:56:02 +0500

    27th

---
+++ 27.js
@@ -0,0 +1,13 @@
+var hp = 40;
+if (hp >= 80) {
+    console.log('you kill enemy with more then 80% hp');
+}
+else if (hp >= 60) {
+    console.log('you kill enemy with more then 60% hp');
+}
+else if (hp >= 40) {
+    console.log('you kill enemy with more then 40% hp');
+}
+else {
+    console.log('Your hp less then 40% .is not abel for fighting with enemy');
+}
---
+++ 27.ts
@@ -0,0 +1,19 @@
+
+let hp=40;
+
+if(hp>=80)
+    {
+        console.log('your hp is able to')
+    }
+else if(hp>=60)
+    {
+        console.log('your hp is able to fight')
+    }
+else if(hp>=40)
+    {
+        console.log('your hp is able to fight')
+    }    
+else
+    {
+        console.log('Your hp less then 40% .is not able for fighting with enemy')
+    }
