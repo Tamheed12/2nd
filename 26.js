1dea01274e031d86df5a86d95d7d1f96ea11faba
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-23 20:55:30 +0500

    26th

---
+++ 26.js
@@ -0,0 +1,7 @@
+var enemy = 'Kill';
+if (enemy === 'Kill') {
+    console.log('you kill enemy so player level up');
+}
+else {
+    console.log('You not kill the enemy so level is not up');
+}
---
+++ 26.ts
@@ -0,0 +1,11 @@
+
+let enemy='Kill';
+
+if(enemy==='Kill')
+    {
+        console.log('you kill enemy so player level up')
+    }
+else
+    {
+        console.log('You not kill the enemy so level is not up')
+    }
