d26072a819cd22b2d6af41aae3b4d7c00669a6cd
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-23 20:56:33 +0500

    28th

---
+++ 28.js
@@ -0,0 +1,23 @@
+var age = 18;
+console.log('\nstage of life by william shakespeare\n');
+if (age <= 4) {
+    console.log(age + ' Infant Mewling and puking in nurse`s arms.');
+}
+else if (age <= 16) {
+    console.log(age + ' Whining schoolboy with his satchel and shining morning face.');
+}
+else if (age <= 26) {
+    console.log(age + ' Lover sighing like furnace with woeful ballad made to his mistress');
+}
+else if (age <= 40) {
+    console.log(age + ' Soldier full of strange oaths and bearded like pard, jealous in honour, sudden and quick in quarrel, seeking bubble reputation');
+}
+else if (age <= 50) {
+    console.log(age + ' Justice good capon lin`d with eye servere and beard of formal cut modern instance');
+}
+else if (age <= 70) {
+    console.log(age + ' Lean spectacles on nose and pouch on side his youthful hose well saved');
+}
+else {
+    console.log(age + ' Last stage evenful history is second childness and mere oblivison sana teeth, sans eyes, sans taste, sans eveything');
+}
