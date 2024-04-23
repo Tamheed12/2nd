8f4aea5907ae86503ae50d6dbfd2869420c52dfb
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-23 20:54:48 +0500

    25th

---
+++ 25.js
@@ -0,0 +1,6 @@
+//assign variable
+var color = 'green';
+//using if funtion
+if (color === 'green') {
+    console.log('The player level up');
+}
---
+++ 25.ts
@@ -0,0 +1,11 @@
+//assign variable
+
+let color='Enemy kill';
+
+//using if funtion
+
+if(color==='Enemy kill')
+{
+    console.log('The player level up');
+
+}
