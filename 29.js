fe3d001781f9eda41a4e99996b55838627874dea
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-23 20:57:02 +0500

    29th

---
+++ 29.js
@@ -0,0 +1,10 @@
+/*
+    In this code all name are taken from
+        Naruto Anime
+*/
+var favoite = ['Itachi', 'Obito', 'Madara', 'Kakashi'];
+for (var i = 0; i < favoite.length; i++) {
+    console.log(favoite[i]);
+    console.log("\nThis is ".concat(favoite[i], " charcter from Naruto Anime and i like it"));
+}
+console.log('\nI like watching Anime and Manga');
---
+++ 29.ts
@@ -0,0 +1,16 @@
+
+
+/*
+    In this code all name are taken from
+        Naruto Anime 
+*/
+
+
+let favoite:string[]=['Itachi','Obito','Madara','Kakashi'];
+
+
+for(let i=0; i<favoite.length;i++)
+    {console.log(favoite[i])
+    console.log(`\nThis is ${favoite[i]} charcter from Naruto Anime and i like it`)}
+    
+    console.log('\nI like watching Anime and Manga')
