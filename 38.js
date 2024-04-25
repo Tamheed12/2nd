284be64936831d1507692eb5263255a73ebc0dba
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-25 11:25:26 +0500

    38th

---
+++ 38.js
@@ -0,0 +1,9 @@
+//make function
+function cities(city, country) {
+    if (country === void 0) { country = 'Pakistan'; }
+    console.log("".concat(city, " city is in ").concat(country, " country"));
+}
+//print first
+cities('Sukkur');
+//print second
+cities('Dahli', 'India');
---
+++ 38.ts
@@ -0,0 +1,13 @@
+
+//make function
+function cities(city:string,country:string='Pakistan')
+{
+    console.log(`${city} city is in ${country} .`)
+}
+
+//print first
+cities('Sukkur');
+
+//print second
+cities('Dahli','India');
+
