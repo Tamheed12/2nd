171afbbed5830b8058608d7ed6bd9bb959012cd3
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-25 10:57:29 +0500

    34th

---
+++ 34.js
@@ -0,0 +1,7 @@
+// i use cities name at the place of pizza name
+var cities = ['Lahore', 'Karachi', 'Sukkur', 'Sibi'];
+for (var _i = 0, cities_1 = cities; _i < cities_1.length; _i++) {
+    var city = cities_1[_i];
+    console.log("I visited ".concat(city, " city of Pakistan"));
+}
+console.log("I love Pakistan and it's beautiful cities");
---
+++ 34.ts
@@ -0,0 +1,10 @@
+
+// i use cities name at the place of pizza name
+let cities=['Lahore','Karachi','Sukkur','Sibi']
+
+for(let city of cities)
+{
+    console.log(`\nI visited ${city} city of Pakistan`)
+}
+
+console.log(`\nI love Pakistan and it's beautiful cities`)
