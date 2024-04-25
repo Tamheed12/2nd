8328d3a159576a7a73fd227efa2804e13f097577
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-25 11:00:43 +0500

    35th

---
+++ 35.js
@@ -0,0 +1,6 @@
+var Animal = ['Cat', 'Dog', 'Rabbit', 'Bird'];
+for (var _i = 0, Animal_1 = Animal; _i < Animal_1.length; _i++) {
+    var pet = Animal_1[_i];
+    console.log("A ".concat(pet, " would make a great pet"));
+}
+console.log("These all animal would make great pet");
---
+++ 35.ts
@@ -0,0 +1,11 @@
+
+let Animal=['Cat','Dog','Rabbit','Bird'] 
+
+
+for(let pet of Animal)
+{
+    console.log(`A ${pet} would make a great pet`)
+}
+
+
+console.log(`\nThese all animal would make great pet`)
