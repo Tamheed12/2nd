1e6dfc414a2fea80553c5b1e4c8d9de73c85ef2c
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-25 11:23:56 +0500

    36th

---
+++ 36.js
@@ -0,0 +1,5 @@
+//make funtion
+function shirt(size, print) {
+    console.log("You select ".concat(size, " size shirt with '").concat(print, "' print on shirt"));
+}
+shirt('Medium', 'I am a Coder');
---
+++ 36.ts
@@ -0,0 +1,13 @@
+
+//make funtion
+function shirt (size :string, print: string)
+{
+    console.log(`You select ${size} size shirt with '${print}' print on shirt`)
+}
+
+//you use these size to check code
+
+//shirt('Small','Work hard')
+shirt('Medium','I am a Coder')
+//shirt('Larege','Don't give up')
+
