01505e74ed7b841aee0b415a413d7c98d188fdd3
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-28 13:09:07 +0500

    39th

---
+++ 39.js
@@ -0,0 +1,5 @@
+//make the function with return
+function cityCountry(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+console.log(cityCountry('Lahore', 'Pakistan'));
---
+++ 39.ts
@@ -0,0 +1,11 @@
+
+//make the function with return
+function cityCountry(city:string,country:string):string
+{
+   return`${city} ${country}`; 
+}
+
+
+console.log(cityCountry('Tokyo','Japan'));
+console.log(cityCountry('Sibi','Pakistan'));
+console.log(cityCountry('Berlin','Germany'));
