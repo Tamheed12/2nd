b565541625bf3600272edac68cb77c9b16757e28
Tamheed12 <162009770+Tamheed12@users.noreply.github.com>
2024-04-28 13:10:27 +0500

    41th

---
+++ 41.js
@@ -0,0 +1,7 @@
+function showMagicians(Magicians) {
+    Magicians.forEach(function (element) {
+        return console.log(element);
+    });
+}
+var Magicians_names = ['Afzal', 'Atif', 'Asif'];
+showMagicians(Magicians_names);
---
+++ 41.ts
@@ -0,0 +1,12 @@
+
+
+function showMagicians(Magicians:string[])
+{
+    Magicians.forEach(element => 
+        console.log(element));
+}
+
+
+let Magicians_names=['Afzal','Atif','Asif']
+
+showMagicians(Magicians_names)
